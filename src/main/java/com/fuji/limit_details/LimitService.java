9717e49a7514b29cd03b32e21d231a203cf3176d
unknown <maruthi.sayampu@gmail.com>
2021-11-29 19:06:29 +0800

    phase-2 changes for service adapter

--- src/main/java/com/fuji/limit_details/LimitService.java
+++ src/main/java/com/fuji/limit_details/LimitService.java
@@ -1,2 +1,9 @@
-package com.fuji.limit_details;public interface LimitService {
+package com.fuji.limit_details;
+
+import com.fuji.limit_adapter.LimitRequest;
+import com.fuji.limit_adapter.UpdateLimits;
+
+public interface LimitService {
+    public UpdateLimits utilizeLimits(LimitRequest limitRequest);
+    public UpdateLimits reverseLimits(LimitRequest limitRequest);
 }
--- src/main/java/com/fuji/limit_details/LimitServiceAdapter.java
+++ src/main/java/com/fuji/limit_details/LimitServiceAdapter.java
@@ -1,2 +1,16 @@
-package com.fuji.limit_details;public class LimitServiceAdapter {
+package com.fuji.limit_details;
+
+import com.fuji.limit_adapter.LimitRequest;
+import com.fuji.limit_adapter.UpdateLimits;
+
+public class LimitServiceAdapter implements LimitService {
+    @Override
+    public UpdateLimits utilizeLimits(LimitRequest limitRequest) {
+        return UpdateLimits.builder().build();
+    }
+
+    @Override
+    public UpdateLimits reverseLimits(LimitRequest limitRequest) {
+        return UpdateLimits.builder().build();
+    }
 }
